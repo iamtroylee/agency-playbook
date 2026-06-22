65ecbeae170185a3d3da68c5e1573592a2607b31
iamtroylee <mr.troyjordan@gmail.com>
2026-06-21 22:08:09 -0700

    Add files via upload

---
+++ GitHub Playbook/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'playbook-v7';
+const SHELL = ['./agency_playbook_v7.html','./manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if(e.request.url.includes('firestore')||e.request.url.includes('firebase'))return;
+  e.respondWith(
+    fetch(e.request).then(res=>{
+      if(SHELL.some(s=>e.request.url.includes(s.replace('./','')))){
+        caches.open(CACHE_NAME).then(c=>c.put(e.request,res.clone()));
+      }
+      return res;
+    }).catch(()=>caches.match(e.request))
+  );
+});
