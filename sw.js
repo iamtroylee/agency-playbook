d0a67821f22b662f80af49ba808623fb87671a0c
iamtroylee <mr.troyjordan@gmail.com>
2026-06-21 23:50:59 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,31 @@
 const CACHE_NAME = 'playbook-v7';
-const SHELL = ['./agency_playbook_v7.html','./manifest.json'];
+// Fixed the file name to index.html and added root '/' paths for robust iOS mapping
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(SHELL)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
-  if(e.request.url.includes('firestore')||e.request.url.includes('firebase'))return;
+  if(e.request.url.includes('firestore') || e.request.url.includes('firebase')) return;
   e.respondWith(
-    fetch(e.request).then(res=>{
-      if(SHELL.some(s=>e.request.url.includes(s.replace('./','')))){
-        caches.open(CACHE_NAME).then(c=>c.put(e.request,res.clone()));
+    fetch(e.request).then(res => {
+      if(SHELL.some(s => e.request.url.includes(s.replace('./', '')))) {
+        caches.open(CACHE_NAME).then(c => c.put(e.request, res.clone()));
       }
       return res;
-    }).catch(()=>caches.match(e.request))
+    }).catch(() => caches.match(e.request))
   );
 });
